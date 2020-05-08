fb395496b2a1be4394388c913cc409a9fd3e4796
Tom Ritchford <tom@swirly.com>
2020-05-08 16:29:30 +0200

    Money conversions; more tests

--- pf.py
+++ pf.py
@@ -1,6 +1,45 @@
 from datetime import datetime
 
-DATE_FORMAT = '%Y/%m/%d:%H:%M:%S/%f'
+DATE_FORMAT = '%Y/%m/%d %H:%M:%S.%f'
+# Includes ms so the roundtrip test works.  I will probably turn that off for
+# the test.
+
+DOLLARS_FORMAT_ERROR = (
+    'Dollars must either be a whole number or have exactly two decimal places'
+)
+
+
+# it is a crime against nature to use floating point for calculations on money.
+# Represent money in whole number cents for these exercises.
+
+def string_to_cents(s):
+    """
+    Convert a string representing a dollar value into a whole number of cents.
+
+    Inputs are either whole numbers (100, -123) or numbers with exactly two
+    digits after the decimal place (100.23, -123.05).
+    """
+    dollars, *cents = s.split('.')
+
+    if len(cents) > 1:
+        raise ValueError(DOLLARS_FORMAT_ERROR)
+
+    cents = cents[0] if cents else '00'
+    if len(cents) != 2 or not cents.isnumeric():
+        raise ValueError(DOLLARS_FORMAT_ERROR)
+
+    try:
+        return 100 * int(dollars) + int(cents)
+    except ValueError:
+        raise ValueError(DOLLARS_FORMAT_ERROR)
+
+
+def cents_to_string(cents):
+    """
+    Convert an integer number of cents into a string representing a dollar
+    value.
+    """
+    return '%d.%02d' % divmod(cents, 100)
 
 
 def string_to_date(s):
@@ -8,4 +47,10 @@ def string_to_date(s):
 
 
 def date_to_string(dt):
+    """"
+    Converts a datetime to a string.
+
+    ARGUMENTS
+      dt: a datetime
+    """
     return dt.strftime(DATE_FORMAT)
--- test_pf.py
+++ test_pf.py
@@ -1,10 +1,31 @@
 from unittest import TestCase
 from datetime import datetime
-from pf import date_to_string, string_to_date
+
+import pf
 
 
 class TestPf(TestCase):
     def test_dates(self):
         now = datetime.now()
-        round_trip = string_to_date(date_to_string(now))
-        self.assertEqual(now, round_trip)
+        now2 = pf.string_to_date(pf.date_to_string(now))
+        self.assertEqual(now, now2)
+
+        dt = datetime(2020, 5, 9, 15, 13, 28, 12345)
+        self.assertEqual(pf.date_to_string(dt), '2020-05-08 15:13:28.12345')
+
+        s = '2020/02/02 03:03:03.00000'
+        s2 = pf.date_to_string(pf.string_to_date(s))
+        self.assertEqual(s, s2)
+
+    def test_dollars(self):
+        for s in '10.00', '-123.05':
+            cents = pf.string_to_cents(s)
+            s2 = pf.cents_to_string(cents)
+            self.assertEqual(s, s2)
+
+        self.assertEqual(pf.string_to_cents('10'), 1000)
+        self.assertEqual(pf.cents_to_string(1000), '10.00')
+
+        for s in '', '.', '0.', '.0', '0.1', '0.-1', '0.1-', '0.123', '0x100':
+            with self.assertRaises(ValueError):
+                pf.string_to_cents(s)
